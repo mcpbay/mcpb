de91010d731d40bb499cb865fbbc40d671ed8403
Iuriiiiiii <alex.casas@webtroniclabs.com>
2026-02-18 07:31:05 -0300

    fix: start-mcp command

--- src/commands/start-mcp.command.ts
+++ src/commands/start-mcp.command.ts
@@ -1,13 +1,13 @@
 import { StdioTransport } from "@mcpbay/easy-mcp-server/transports";
 import { McpServerContext } from "../classes/mcp-server-context.class.ts";
 import { ContextVersion } from "../types/context-version.type.ts";
-import { checkConfigFileExists } from "../utils/check-config-file-exists.util.ts";
 import { loadContext } from "../utils/load-context.util.ts";
 import { EasyMCPServer } from "@mcpbay/easy-mcp-server";
+import { loadOrCreateConfigFile } from "../utils/load-or-create-config-file.util.ts";
 
 export async function startMcpCommand(options: Record<string, any>) {
   const { config: configPath } = options;
-  const config = checkConfigFileExists(configPath);
+  const config = loadOrCreateConfigFile(configPath);
   const contexts: ContextVersion[] = [];
 
   for (const [slug, version] of Object.entries(config.imports)) {
